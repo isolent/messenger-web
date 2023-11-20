1cdc7805af76131285d313c7d6ed205dcd3cb1d2
isolent <a_kakharmanova@kbtu.kz>
2023-11-21 00:07:20 +0600

    website

---
+++ messenger-web/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import Messenger from './Messenger';
+
+function App() {
+  return (
+    <div>
+      <Messenger />
+    </div>
+  );
+}
+
+export default App;
